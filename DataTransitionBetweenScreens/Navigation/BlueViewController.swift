b649dd076ed704ab795b0832619b611a0f180e73
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-18 23:15:33 -0300

    transition of object data between screens

--- DataTransitionBetweenScreens/Navigation/BlueViewController.swift
+++ DataTransitionBetweenScreens/Navigation/BlueViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class BlueViewController: UIViewController {
     
-    var name = ""
-    var surname = ""
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var professionLabel: UILabel!
 
+    var person: Person?
+    
     @IBOutlet weak var fullNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fullNameLabel.text = "\(name) \(surname)"
+        firstNameLabel.text = person?.firstName
+        lastNameLabel.text = person?.lastName
+        ageLabel.text = person?.age
+        professionLabel.text = person?.profession
     }
     
     @IBAction func callYellowScreenButtonTapped(_ sender: Any) {
@@ -26,5 +33,4 @@ class BlueViewController: UIViewController {
     @IBAction func dismissScreenButtonTapped(_ sender: Any) {
         dismiss(animated: true)
     }
-    
 }
---
+++ DataTransitionBetweenScreens/Navigation/MyStructFile.swift
@@ -0,0 +1,15 @@
+//
+//  MyStructFile.swift
+//  Navigation
+//
+//  Created by Anderson Sales on 18/10/22.
+//
+
+import Foundation
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    var age: String
+    var profession: String
+}
