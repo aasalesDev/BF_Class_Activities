09ad28ed702e33e1cd858c25bbc90c8c2ac72489
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-20 22:21:28 -0300

    created an XIB with its element

---
+++ TableViewExercise/TableViewExercise/Cell/NameTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  NameTableViewCell.swift
+//  TableViewExercise
+//
+//  Created by Anderson Sales on 20/10/22.
+//
+
+import UIKit
+
+class NameTableViewCell: UITableViewCell {
+    
+    /*
+     1 - Create cell
+     2 - create identifier
+     3 - Add indentifier to the XIB
+     4 - Create NIB method
+     5 - Create elements and link them
+     */
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    static let identifier: String = "NameTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+
+    // Cell life cycle (like the viewDidLoad())
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+}
--- TableViewExercise/TableViewExercise/ViewController.swift
+++ TableViewExercise/TableViewExercise/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
     }
-
-
+    
+    func configureTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
 
+extension ViewController: UITableViewDelegate{
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    // indicates the quantity of rows per section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // populates each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+}
