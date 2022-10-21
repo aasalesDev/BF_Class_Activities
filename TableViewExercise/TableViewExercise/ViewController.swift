12944562b8660f9803d698fd207eb7b7b84c2d72
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-20 21:02:11 -0300

    created the base project - table view

---
+++ TableViewExercise/TableViewExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewExercise
+//
+//  Created by Anderson Sales on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
