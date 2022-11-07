c1783fddbd32d80a5a7c0196ceab211c10fe88ac
Anderson Sales <andersonsales.mg@gmail.com>
2022-11-07 20:28:15 -0300

    base files created

---
+++ FirstCollectionViewApp/FirstCollectionViewApp/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  FirstCollectionViewApp
+//
+//  Created by Anderson Sales on 07/11/22.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- FirstCollectionViewApp/FirstCollectionViewApp/ViewController.swift
+++ FirstCollectionViewApp/FirstCollectionViewApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func configureCollectionView(){
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
 }
 
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100.00, height: 100.00)
+    }
+}
